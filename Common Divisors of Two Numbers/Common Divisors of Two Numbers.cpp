dae63c4a0a3ae22867f8f52bda2c6b98ea71bf9d
Mohammad-Mahdi1999 <44753085+Mohammad-Mahdi1999@users.noreply.github.com>
2018-12-03 22:03:53 +0330

    Used function

--- Common Divisors of Two Numbers/Common Divisors of Two Numbers.cpp
+++ Common Divisors of Two Numbers/Common Divisors of Two Numbers.cpp
@@ -4,17 +4,22 @@
 #include "stdafx.h"
 #include <iostream>
 using namespace std;
-
+void codiv(int x, int y)
+{
+	int i;
+	for (i = 1; i <= x; i++)
+		if (x%i == 0 && y%i == 0)
+			cout << i << endl;
+}
 void main()
 {
-	int x, y, i;
+	int x, y;
 	cout << "Enter first number :";
 	cin >> x;
 	cout << "Enter second number : ";
 	cin >> y;
-	for (i = 1; i <= x; i++)
-		if (x%i == 0 && y%i == 0)
-			cout << i << endl;
+	codiv(x, y);
+	cout << "Press Enter key to exit . . .";
 	cin.get();
 	cin.ignore();
 }
